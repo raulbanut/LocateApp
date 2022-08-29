442c183217a7820b1e112a602d1076a936f99ff0
Banut Raul <banut.raul@yahoo.com>
2022-08-29 12:54:33 +0300

    First commit

---
+++ Locate/Locate/LocateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocateApp.swift
+//  Locate
+//
+//  Created by Raul Banut on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LocateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Locate/Locate/Utilities/Extensions/Color+Custom.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Custom.swift
+//  Locate
+//
+//  Created by Raul Banut on 24.08.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let cedarChest = Color("Cedar_Chest")
+    
+    static let rhythm = Color("Rhythm")
+    
+    static let pewterBlue = Color("Pewter_Blue")
+}
