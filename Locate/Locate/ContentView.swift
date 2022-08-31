8e6e8f80c6806b0f585fb18f4b77a71074c19a65
Banut Raul <banut.raul@yahoo.com>
2022-08-31 14:07:45 +0300

    add navigationStack

--- Locate/Locate/ContentView.swift
+++ Locate/Locate/ContentView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+enum Destinations: Hashable {
+    case onboarding
+    case home
+}
+
+class Coordinator: ObservableObject {
+    @Published var navigationPath = NavigationPath()
+    
+    func addView(for destination: Destinations) {
+        navigationPath.append(destination)
+    }
+}
+
 struct ContentView: View {
+    @StateObject var coordinator = Coordinator()
+    
     var body: some View {
-        BackgroundView()
+        NavigationStack(path: $coordinator.navigationPath) {
+            VStack {
+                Button {
+                    coordinator.addView(for: .home)
+                } label: {
+                    Text("Go home")
+                }
+            }
+            .navigationDestination(for: Destinations.self) { item in
+                if item == .home {
+                    MainCoordinator()
+                        .hideNavigationBar()
+                        .environmentObject(coordinator)
+                } else {
+                    OnboardingCoordinator()
+                        .hideNavigationBar()
+                        .environmentObject(coordinator)
+                    
+                }
+            }
+        }
+        
+    }
+}
+
+extension View {
+    func hideNavigationBar() -> some View {
+        self.navigationBarBackButtonHidden()
+            .navigationTitle("")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
---
+++ Locate/Locate/MainCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  MainCoordinator.swift
+//  Locate
+//
+//  Created by Raul Banut on 30.08.2022.
+//
+
+import SwiftUI
+
+struct MainCoordinator: View {
+    @EnvironmentObject var coordinator : Coordinator
+    
+    var body: some View {
+        ZStack {
+            AuthenticationView()
+            Button {
+                coordinator.addView(for: .onboarding)
+            } label: {
+                Text("Main")
+                    .background(.red)
+            }
+        }
+    }
+}
+
+struct MainCoordinator_Previews: PreviewProvider {
+    static var previews: some View {
+        MainCoordinator()
+    }
+}
