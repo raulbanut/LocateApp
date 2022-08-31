99df5e97819ee138fe5d7449617e43141b5520bb
Banut Raul <banut.raul@yahoo.com>
2022-08-31 14:06:39 +0300

    Refactor BackgroundView

--- Locate/Locate/CustomViews/BackgroundView.swift
+++ Locate/Locate/CustomViews/BackgroundView.swift
@@ -9,45 +9,16 @@ import SwiftUI
 
 struct BackgroundView: View {
     var body: some View {
-        ZStack {
-            BackgroundBody()
-                .foregroundColor(.pewterBlue)
-                .ignoresSafeArea()
-            
-            VStack(spacing: 8) {
-                Spacer()
-                
-                Text("Locate")
-                    .bold()
-                    .foregroundColor(.cedarChest)
-                    .font(.largeTitle)
-                
-                Image(systemName: "globe.europe.africa.fill")
-                    .font(.system(size: 200))
-                    .foregroundColor(.cedarChest)
-                
-                Spacer()
-                
-                TermsView()
-                
-                VStack(spacing: 8) {
-                    ButtonView(text: "Continue with Email", image: Image(systemName: "envelope.fill")) {
-                        
-                    }
-                    ButtonView(text: "Continue with Apple", image: Image(systemName: "applelogo")) {
-                        
-                    }
-                }
-                .padding(8)
-            }
-            .padding()
-        }
+        BackgroundBody()
+            .foregroundColor(.pewterBlue)
+            .ignoresSafeArea()
+        
     }
 }
 
 struct BackgroundView_Previews: PreviewProvider {
     static var previews: some View {
         BackgroundView()
-//            .previewDevice("iPhone SE (3rd generation)")
+        //            .previewDevice("iPhone SE (3rd generation)")
     }
 }
