6b5465b9f62fa6eb75ed9481873ec70f5bc6224a
Banut Raul <banut.raul@yahoo.com>
2022-10-03 21:59:14 +0300

    Add Logger

---
+++ Locate/Locate/Utilities/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  Locate
+//
+//  Created by Raul Banut on 03.10.2022.
+//
+
+import Foundation
+
+struct Logger {
+    static func log(_ string: String) {
+        print("\nPRLY - \(string)")
+    }
+}
